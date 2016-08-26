c1202ebc2411bdc94f4d9134bd18499b282081fa
Hai Fang <hfang.shanghai@gmail.com>
2016-08-26 15:11:25 +0100

    Update to the version 1.0.3

--- R/xGRviaGenomicAnnoAdv.r
+++ R/xGRviaGenomicAnnoAdv.r
@@ -117,12 +117,10 @@
 #' \dontrun{
 #' # Load the library
 #' library(XGR)
-#' #RData.location <- "http://galahad.well.ox.ac.uk/bigdata"
 #' 
 #' # Enrichment analysis for GWAS SNPs from ImmunoBase
 #' # a) provide input data
 #' data.file <- "http://galahad.well.ox.ac.uk/bigdata/ImmunoBase_GWAS.bed"
-#' #data.file <- "~/Sites/SVN/github/bigdata/ImmunoBase_GWAS.bed"
 #' 
 #' # b) perform enrichment analysis using FANTOM expressed enhancers
 #' eTerm1 <- xGRviaGenomicAnnoAdv(data.file=data.file, format.file="bed", GR.annotation="FANTOM5_Enhancer_Cell", num.samples=1000, gap.max=50000)
