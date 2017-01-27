d736d051f171b0b55f64eb1163a762fbb10ea125
Hai Fang <hfang.shanghai@gmail.com>
2017-01-27 11:31:10 +0000

    Update to the version 1.0.9

--- R/xEnricherGenes.r
+++ R/xEnricherGenes.r
@@ -169,97 +169,14 @@ xEnricherGenes <- function(data, background=NULL, ontology=c("GOBP","GOMF","GOCC
 		stop("There is no input for the ontology.\n")
 	}   
     
-    #############################################################################################
-    # A function converting from symbol to entrezgene
-    symbol2entrezgene <- function(Symbol, check.symbol.identity, allGeneID, allSymbol, allSynonyms, verbose){
-    
-        ## correct for those symbols being shown as DATE format
-        if(1){
-            ## for those starting with 'Mar' in a excel-input date format
-            a <- Symbol
-            flag <- grep("-Mar$", a, ignore.case=T, perl=T, value=F)
-            if(length(flag)>=1){
-                b <- a[flag]
-                c <- sub("-Mar$", "", b, ignore.case=T, perl=T)
-                d <- sub("^0", "", c, ignore.case=T, perl=T)
-                e <- sapply(d, function(x) paste(c("March",x), collapse=""))
-                a[flag] <- e
-                Symbol <- a
-            }
-
-            ## for those starting with 'Sep' in a excel-input date format
-            a <- Symbol
-            flag <- grep("-Sep$", a, ignore.case=T, perl=T, value=F)
-            if(length(flag)>=1){
-                b <- a[flag]
-                c <- sub("-Sep$", "", b, ignore.case=T, perl=T)
-                d <- sub("^0", "", c, ignore.case=T, perl=T)
-                e <- sapply(d, function(x) paste(c("Sept",x), collapse=""))
-                a[flag] <- e
-                Symbol <- a
-            }
-        }
-        
-        ## case-insensitive
-        match_flag <- match(tolower(Symbol),tolower(allSymbol))
-        
-        ## match vis Synonyms for those unmatchable by official gene symbols
-        if(check.symbol.identity){
-            ## match Synonyms (if not found via Symbol)
-            na_flag <- is.na(match_flag)
-            a <- Symbol[na_flag]
-
-            ###
-            tmp_flag <- is.na(match(tolower(allSymbol), tolower(Symbol)))
-            tmp_Synonyms <- allSynonyms[tmp_flag]
-            Orig.index <- seq(1,length(allSynonyms))
-            Orig.index <- Orig.index[tmp_flag]
-            ###
-
-            b <- sapply(1:length(a), function(x){
-                tmp_pattern1 <- paste("^",a[x],"\\|", sep="")
-                tmp_pattern2 <- paste("\\|",a[x],"\\|", sep="")
-                tmp_pattern3 <- paste("\\|",a[x],"$", sep="")
-                tmp_pattern <- paste(tmp_pattern1,"|",tmp_pattern2,"|",tmp_pattern3, sep="")
-                tmp_result <- grep(tmp_pattern, tmp_Synonyms, ignore.case=T, perl=T, value=F)
-                ifelse(length(tmp_result)==1, Orig.index[tmp_result[1]], NA)
-            })
-            match_flag[na_flag] <- b
-            
-            if(verbose){
-                now <- Sys.time()
-                message(sprintf("\tAmong %d symbols of input data, there are %d mappable via official gene symbols, %d mappable via gene alias but %d left unmappable", length(Symbol), (length(Symbol)-length(a)), sum(!is.na(b)), sum(is.na(b))), appendLF=T)
-            }
-        }else{
-            if(verbose){
-                now <- Sys.time()
-                message(sprintf("\tAmong %d symbols of input data, there are %d mappable via official gene symbols but %d left unmappable", length(Symbol), (sum(!is.na(match_flag))), (sum(is.na(match_flag)))), appendLF=T)
-            }
-        
-        }
-        
-        ## convert into GeneID
-        GeneID <- allGeneID[match_flag]
-        
-        return(GeneID)
-    }
-    #############################################################################################
-    
     ## convert gene symbol to entrz gene for both input data of interest and the input background (if given)
     if(verbose){
 		now <- Sys.time()
 		message(sprintf("Do gene mapping from Symbols to EntrezIDs (%s) ...", as.character(now)), appendLF=T)
 	}
-    
-    ## load Enterz Gene information
-	EG <- xRDataLoader(RData.customised=paste('org.Hs.eg', sep=''), RData.location=RData.location, verbose=verbose)	
-    
-	allGeneID <- EG$gene_info$GeneID
-	allSymbol <- as.vector(EG$gene_info$Symbol)
-	allSynonyms <- as.vector(EG$gene_info$Synonyms)
-    data <- symbol2entrezgene(Symbol=data, check.symbol.identity=F, allGeneID=allGeneID, allSymbol=allSymbol, allSynonyms=allSynonyms, verbose=verbose)
+    data <- xSymbol2GeneID(data, check.symbol.identity=F, verbose=verbose, RData.location=RData.location)
     if(length(background)>0){
-        background <- symbol2entrezgene(Symbol=background, check.symbol.identity=F, allGeneID=allGeneID, allSymbol=allSymbol, allSynonyms=allSynonyms, verbose=verbose)
+        background <- xSymbol2GeneID(background, check.symbol.identity=F, verbose=verbose, RData.location=RData.location)
     }
     
     #############################################################################################
@@ -274,6 +191,11 @@ xEnricherGenes <- function(data, background=NULL, ontology=c("GOBP","GOMF","GOCC
 	
 	# replace EntrezGenes with gene symbols	
 	if(1 & class(eTerm)=="eTerm"){
+		## load Enterz Gene information
+		EG <- xRDataLoader(RData.customised=paste('org.Hs.eg', sep=''), RData.location=RData.location, verbose=verbose)
+		allGeneID <- EG$gene_info$GeneID
+		allSymbol <- as.vector(EG$gene_info$Symbol)
+	
 		## overlap
 		overlap <- eTerm$overlap
 		overlap_symbols <- lapply(overlap,function(x){
--- R/xSocialiserGenes.r
+++ R/xSocialiserGenes.r
@@ -1,6 +1,6 @@
 #' Function to calculate pair-wise semantic similarity given a list of genes and the ontology in query
 #'
-#' \code{xSocialiserGenes} is supposed to calculate pair-wise semantic similarity between a list of input genes and the ontology in query. It returns an object of class "igraph", a network representation of socialized genes. Now it supports enrichment analysis using a wide variety of ontologies such as Gene Ontology and Phenotype Ontologies. It first calculates semantic similarity between terms and then derives semantic similarity from term-term semantic similarity. Parallel computing is also supported for Linux or Mac operating systems.
+#' \code{xSocialiserGenes} is supposed to calculate pair-wise semantic similarity between a list of input genes and the ontology in query. It returns an object of class "igraph", a network representation of socialized genes. Now it supports enrichment analysis using a wide variety of ontologies such as Gene Ontology and Phenotype Ontologies. It first calculates semantic similarity between terms and then derives semantic similarity from term-term semantic similarity. Parallel computing is also supported.
 #'
 #' @param data an input vector. It contains a list of Gene Symbols of interest
 #' @param ontology the ontology supported currently. It can be "GOBP" for Gene Ontology Biological Process, "GOMF" for Gene Ontology Molecular Function, "GOCC" for Gene Ontology Cellular Component, "DO" for Disease Ontology, "HPPA" for Human Phenotype Phenotypic Abnormality, "HPMI" for Human Phenotype Mode of Inheritance, "HPCM" for Human Phenotype Clinical Modifier, "HPMA" for Human Phenotype Mortality Aging, "MP" for Mammalian Phenotype
@@ -9,7 +9,7 @@
 #' @param rescale logical to indicate whether the resulting values are rescaled to the range [0,1]. By default, it sets to true
 #' @param force logical to indicate whether the only most specific terms (for each SNP) will be used. By default, it sets to true. It is always advisable to use this since it is computationally fast but without compromising accuracy (considering the fact that true-path-rule has been applied when running \code{\link{xDAGanno}})
 #' @param fast logical to indicate whether a vectorised fast computation is used. By default, it sets to true. It is always advisable to use this vectorised fast computation; since the conventional computation is just used for understanding scripts
-#' @param parallel logical to indicate whether parallel computation with multicores is used. By default, it sets to true, but not necessarily does so. Partly because parallel backends available will be system-specific (now only Linux or Mac OS). Also, it will depend on whether these two packages "foreach" and "doMC" have been installed. It can be installed via: \code{source("http://bioconductor.org/biocLite.R"); biocLite(c("foreach","doMC"))}. If not yet installed, this option will be disabled
+#' @param parallel logical to indicate whether parallel computation with multicores is used. By default, it sets to true, but not necessarily does so. It will depend on whether these two packages "foreach" and "doParallel" have been installed. It can be installed via: \code{source("http://bioconductor.org/biocLite.R"); biocLite(c("foreach","doParallel"))}. If not yet installed, this option will be disabled
 #' @param multicores an integer to specify how many cores will be registered as the multicore parallel backend to the 'foreach' package. If NULL, it will use a half of cores available in a user's computer. This option only works when parallel computation is enabled
 #' @param path.mode the mode of paths induced by vertices/nodes with input annotation data. It can be "all_paths" for all possible paths to the root, "shortest_paths" for only one path to the root (for each node in query), "all_shortest_paths" for all shortest paths to the root (i.e. for each node, find all shortest paths with the equal lengths)
 #' @param verbose logical to indicate whether the messages will be displayed in the screen. By default, it sets to false for no display
@@ -98,95 +98,12 @@ xSocialiserGenes <- function(data, ontology=c("GOBP","GOMF","GOCC","DO","HPPA","
 		stop("There is no input for the ontology.\n")
 	}   
     
-    #############################################################################################
-    # A function converting from symbol to entrezgene
-    symbol2entrezgene <- function(Symbol, check.symbol.identity, allGeneID, allSymbol, allSynonyms, verbose){
-    
-        ## correct for those symbols being shown as DATE format
-        if(1){
-            ## for those starting with 'Mar' in a excel-input date format
-            a <- Symbol
-            flag <- grep("-Mar$", a, ignore.case=T, perl=T, value=F)
-            if(length(flag)>=1){
-                b <- a[flag]
-                c <- sub("-Mar$", "", b, ignore.case=T, perl=T)
-                d <- sub("^0", "", c, ignore.case=T, perl=T)
-                e <- sapply(d, function(x) paste(c("March",x), collapse=""))
-                a[flag] <- e
-                Symbol <- a
-            }
-
-            ## for those starting with 'Sep' in a excel-input date format
-            a <- Symbol
-            flag <- grep("-Sep$", a, ignore.case=T, perl=T, value=F)
-            if(length(flag)>=1){
-                b <- a[flag]
-                c <- sub("-Sep$", "", b, ignore.case=T, perl=T)
-                d <- sub("^0", "", c, ignore.case=T, perl=T)
-                e <- sapply(d, function(x) paste(c("Sept",x), collapse=""))
-                a[flag] <- e
-                Symbol <- a
-            }
-        }
-        
-        ## case-insensitive
-        match_flag <- match(tolower(Symbol),tolower(allSymbol))
-        
-        ## match vis Synonyms for those unmatchable by official gene symbols
-        if(check.symbol.identity){
-            ## match Synonyms (if not found via Symbol)
-            na_flag <- is.na(match_flag)
-            a <- Symbol[na_flag]
-
-            ###
-            tmp_flag <- is.na(match(tolower(allSymbol), tolower(Symbol)))
-            tmp_Synonyms <- allSynonyms[tmp_flag]
-            Orig.index <- seq(1,length(allSynonyms))
-            Orig.index <- Orig.index[tmp_flag]
-            ###
-
-            b <- sapply(1:length(a), function(x){
-                tmp_pattern1 <- paste("^",a[x],"\\|", sep="")
-                tmp_pattern2 <- paste("\\|",a[x],"\\|", sep="")
-                tmp_pattern3 <- paste("\\|",a[x],"$", sep="")
-                tmp_pattern <- paste(tmp_pattern1,"|",tmp_pattern2,"|",tmp_pattern3, sep="")
-                tmp_result <- grep(tmp_pattern, tmp_Synonyms, ignore.case=T, perl=T, value=F)
-                ifelse(length(tmp_result)==1, Orig.index[tmp_result[1]], NA)
-            })
-            match_flag[na_flag] <- b
-            
-            if(verbose){
-                now <- Sys.time()
-                message(sprintf("\tAmong %d symbols of input data, there are %d mappable via official gene symbols, %d mappable via gene alias but %d left unmappable", length(Symbol), (length(Symbol)-length(a)), sum(!is.na(b)), sum(is.na(b))), appendLF=T)
-            }
-        }else{
-            if(verbose){
-                now <- Sys.time()
-                message(sprintf("\tAmong %d symbols of input data, there are %d mappable via official gene symbols but %d left unmappable", length(Symbol), (sum(!is.na(match_flag))), (sum(is.na(match_flag)))), appendLF=T)
-            }
-        
-        }
-        
-        ## convert into GeneID
-        GeneID <- allGeneID[match_flag]
-        
-        return(GeneID)
-    }
-    #############################################################################################
-    
-    ## convert gene symbol to entrz gene for both input data of interest and the input background (if given)
+    ## convert gene symbol to entrz gene for both input data of interest
     if(verbose){
 		now <- Sys.time()
 		message(sprintf("Do gene mapping from Symbols to EntrezIDs for (%s) ...", as.character(now)), appendLF=T)
 	}
-    
-    ## load Enterz Gene information
-	EG <- xRDataLoader(RData=paste('org.Hs.eg', sep=''), RData.location=RData.location, verbose=verbose)	
-    
-	allGeneID <- EG$gene_info$GeneID
-	allSymbol <- as.vector(EG$gene_info$Symbol)
-	allSynonyms <- as.vector(EG$gene_info$Synonyms)
-    data <- symbol2entrezgene(Symbol=data, check.symbol.identity=F, allGeneID=allGeneID, allSymbol=allSymbol, allSynonyms=allSynonyms, verbose=verbose)
+    data <- xSymbol2GeneID(data, check.symbol.identity=F, verbose=verbose, RData.location=RData.location)
     
     #############################################################################################
     
@@ -209,6 +126,11 @@ xSocialiserGenes <- function(data, ontology=c("GOBP","GOMF","GOCC","DO","HPPA","
     ## the resulting graph has gene symbols (instead of Entrez GeneIDs) as nodes
     if(!is.null(res)){
     	dag <- res$dag
+    	
+		## load Enterz Gene information
+		EG <- xRDataLoader(RData.customised=paste('org.Hs.eg', sep=''), RData.location=RData.location, verbose=verbose)
+		allGeneID <- EG$gene_info$GeneID
+		allSymbol <- as.vector(EG$gene_info$Symbol)
     
     	sim_ig <- res
     	ind <- match(V(sim_ig)$name, allGeneID)
