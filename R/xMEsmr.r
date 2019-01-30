f5e8a06dc886a045125774793908394bfabc9fca
Hai Fang <hfang.shanghai@gmail.com>
2019-01-30 19:09:15 +0000

    Update to the version 1.1.5

--- R/xMEsmr.r
+++ R/xMEsmr.r
@@ -1,37 +1,25 @@
-#' Function to conduct SMR analysis
+#' Function to conduct colocalisation analysis through SMR
 #'
-#' \code{xMEsmr} is supposed to conduct conduct SMR analysis.
+#' \code{xMEsmr} is supposed to conduct conduct Summary-data-based Mendelian Randomisation (SMR) integrating GWAS and eQTL summary data.
 #'
-#' @param data.file an input data file, containing a list of entities (e.g. genes or SNPs) to test. The entities can be anything, for example, in this file \url{http://dcgor.r-forge.r-project.org/data/InterPro/InterPro.txt}, the entities are InterPro domains (InterPro). As seen in this example, entries in the first column must be domains. If the file also contains other columns, these additional columns will be ignored. Alternatively, the data.file can be a matrix or data frame, assuming that input file has been read. Note: the file should use the tab delimiter as the field separator between columns
-#' @param annotation.file an input annotation file containing annotations between entities and ontology terms. For example, a file containing annotations between InterPro domains and GO Molecular Function (GOMF) terms can be found in \url{http://dcgor.r-forge.r-project.org/data/InterPro/Domain2GOMF.txt}. As seen in this example, the input file must contain two columns: 1st column for domains, 2nd column for ontology terms. If there are additional columns, these columns will be ignored. Alternatively, the annotation.file can be a matrix or data frame, assuming that input file has been read. Note: the file should use the tab delimiter as the field separator between columns
-#' @param background.file an input background file containing a list of entities as the test background. The file format is the same as 'data.file'. By default, it is NULL meaning all annotatable entities (i.g. those entities in 'annotation.file') are used as background
-#' @param size.range the minimum and maximum size of members of each term in consideration. By default, it sets to a minimum of 10 but no more than 2000
-#' @param min.overlap the minimum number of overlaps. Only those terms with members that overlap with input data at least min.overlap (3 by default) will be processed
-#' @param test the test statistic used. It can be "fisher" for using fisher's exact test, "hypergeo" for using hypergeometric test, or "binomial" for using binomial test. Fisher's exact test is to test the independence between gene group (genes belonging to a group or not) and gene annotation (genes annotated by a term or not), and thus compare sampling to the left part of background (after sampling without replacement). Hypergeometric test is to sample at random (without replacement) from the background containing annotated and non-annotated genes, and thus compare sampling to background. Unlike hypergeometric test, binomial test is to sample at random (with replacement) from the background with the constant probability. In terms of the ease of finding the significance, they are in order: hypergeometric test > fisher's exact test > binomial test. In other words, in terms of the calculated p-value, hypergeometric test < fisher's exact test < binomial test
-#' @param background.annotatable.only logical to indicate whether the background is further restricted to the annotatable (covered by 'annotation.file'). By default, it is NULL: if the background not provided, it will be TRUE; otherwise FALSE. Surely, it can be explicitly stated. Notably, if only one annotation is provided in 'annotation.file', it should be false (also the background.file should be provided)
-#' @param p.tail the tail used to calculate p-values. It can be either "two-tails" for the significance based on two-tails (ie both over- and under-overrepresentation)  or "one-tail" (by default) for the significance based on one tail (ie only over-representation)
+#' @param gwas.summary an input GWAS summary data file, containing columns 'snp', 'effect allele' (the allele assessed), 'other allele', 'freq' (frequency of the effect allele; not essential unless 'freq.check' is true), 'b' (effect size for the allele assessed; log(odds ratio) for a case-control study), 'se' (standard error), 'p' (p-value) adn 'n' (sample size; not required)
+#' @param beqtl.summary a character specifying where to find eQTL summary data files in the BESD format containing three files (.esi for SNP information, .epi for probe information, and .besd for eQTL summary statistics)
+#' @param bfile a character specifying where to find the LD reference data containing three files (.bed, .bim, and .fam)
+#' @param mode a character specifying the SMR and HEIDI test mode. It can be 'cis' for the test in cis regions, 'trans' for the test in trans regions, and 'both' for both regions
+#' @param peqtl eQTL p-value threshold for selecting a probe (with the top associated eQTL passing a p-value threshold) for the SMR test. In other words, a probe with the top associated eQTL not passing this threshold will be removed for the test. By default, it is 5e-8
+#' @param window.cis an integer specifying a window centred around the probe to select cis-eQTLs (passing a p-value threshold) for the SMR test. The default value is 1000Kb
+#' @param window.trans an integer specifying a window centred around the top associated trans-eQTL to select trans-eQTLs (passing a p-value threshold) for the SMR and HEIDI test. The default value is 1000Kb
+#' @param heidi logical to indicate whether the HEIDI test is enabled. By default it is true
+#' @param heidi.peqtl eQTL p-value threshold for selecting eQTLs per probe for the HEIDI test. The default value is 1.57e-3 (a chi-squared value 10 with df=1)
+#' @param heidi.ld LD r2 threshold used to further prune SNPs (eQTLs) in the HEIDI test. By default only those between 0.05 and 0.9 will be used for the test
+#' @param heidi.num the number of SNPs (eQTLs) left per probe in the HEIDI test. By default, the test skipped if the number of remaining SNPs is less than 3; and top 20 SNPs (ranked by eQTL p-value) will be used in the test
+#' @param freq.check logical to indicate whether to remove SNPs withe discrepant allele frequencies between data sets. By default it is disabled
+#' @param thread.num an integer specifying the number of OpenMP threads for parallel computing. By default it is 1
 #' @param p.adjust.method the method used to adjust p-values. It can be one of "BH", "BY", "bonferroni", "holm", "hochberg" and "hommel". The first two methods "BH" (widely used) and "BY" control the false discovery rate (FDR: the expected proportion of false discoveries amongst the rejected hypotheses); the last four methods "bonferroni", "holm", "hochberg" and "hommel" are designed to give strong control of the family-wise error rate (FWER). Notes: FDR is a less stringent condition than FWER
 #' @param verbose logical to indicate whether the messages will be displayed in the screen. By default, it sets to false for no display
 #' @param silent logical to indicate whether the messages will be silent completely. By default, it sets to false. If true, verbose will be forced to be false
 #' @return 
-#' an object of class "eTerm", a list with following components:
-#' \itemize{
-#'  \item{\code{term_info}: a matrix of nTerm X 4 containing snp/gene set information, where nTerm is the number of terms, and the 4 columns are "id" (i.e. "Term ID"), "name" (i.e. "Term Name"), "namespace" and "distance"}
-#'  \item{\code{annotation}: a list of terms containing annotations, each term storing its annotations. Always, terms are identified by "id"}
-#'  \item{\code{g}: an igraph object to represent DAG}
-#'  \item{\code{data}: a vector containing input data in consideration. It is not always the same as the input data as only those mappable are retained}
-#'  \item{\code{background}: a vector containing the background data. It is not always the same as the input data as only those mappable are retained}
-#'  \item{\code{overlap}: a list of overlapped snp/gene sets, each storing snps overlapped between a snp/gene set and the given input data (i.e. the snps of interest). Always, gene sets are identified by "id"}
-#'  \item{\code{fc}: a vector containing fold changes}
-#'  \item{\code{zscore}: a vector containing z-scores}
-#'  \item{\code{pvalue}: a vector containing p-values}
-#'  \item{\code{adjp}: a vector containing adjusted p-values. It is the p value but after being adjusted for multiple comparisons}
-#'  \item{\code{or}: a vector containing odds ratio}
-#'  \item{\code{CIl}: a vector containing lower bound confidence interval for the odds ratio}
-#'  \item{\code{CIu}: a vector containing upper bound confidence interval for the odds ratio}
-#'  \item{\code{cross}: a matrix of nTerm X nTerm, with an on-diagnal cell for the overlapped-members observed in an individaul term, and off-diagnal cell for the overlapped-members shared betwene two terms}
-#'  \item{\code{call}: the call that produced this result}
-#' }
+#' a data frame with following columns ("mode", "probeID", "Gene", "ProbeChr", "Probe_bp", "topSNP", "topSNP_chr", "topSNP_bp", "A1", "A2", "b_GWAS", "b_eQTL", "b_SMR", "p_GWAS", "p_eQTL", "p_SMR", "fdr_SMR", "p_HEIDI", "fdr_HEIDI", "nsnp_HEIDI")
 #' @note None
 #' @export
 #' @seealso \code{\link{xMEsmr}}
@@ -44,10 +32,11 @@
 #' bfile <- "~/Sites/SVN/github/bigdata_dev/Merged_EUR"
 #' beqtl.summary <- "~/Sites/SVN/github/bigdata_dev/Pi_eQTL_hg19/Neutrophil"
 #' gwas.summary <- "summary_gwas.txt"
-#' df_res <- xMEsmr(gwas.summary, beqtl.summary, bfile)
+#' df_output <- xMEsmr(gwas.summary, beqtl.summary, bfile)
+#' utils::write.table(df_output, file="df_output.txt", row.names=F, col.names=T, quote=F, sep="\t")
 #' }
 
-xMEsmr <- function(gwas.summary, beqtl.summary, bfile, test=c("both","cis","trans"), window.cis=1000, window.trans=1000, peqtl=5e-8, ld=c(0.05,0.9), heidi=T, heidi.peqtl=1.57e-3, heidi.num=c(3,20), freq.check=F, thread.num=1, verbose=T, silent=FALSE)
+xMEsmr <- function(gwas.summary, beqtl.summary, bfile, mode=c("both","cis","trans"), peqtl=5e-8, window.cis=1000, window.trans=1000, heidi=T, heidi.peqtl=1.57e-3, heidi.ld=c(0.05,0.9), heidi.num=c(3,20), freq.check=F, thread.num=1, p.adjust.method=c("BH","BY","bonferroni","holm","hochberg","hommel"), verbose=T, silent=FALSE)
 {
     startT <- Sys.time()
     if(!silent){
@@ -60,7 +49,8 @@ xMEsmr <- function(gwas.summary, beqtl.summary, bfile, test=c("both","cis","tran
     ####################################################################################
     
     ## match.arg matches arg against a table of candidate values as specified by choices, where NULL means to take the first one
-    test <- match.arg(test)
+    mode <- match.arg(mode)
+    p.adjust.method <- match.arg(p.adjust.method)
     
     ############
     ## bfile
@@ -89,9 +79,9 @@ xMEsmr <- function(gwas.summary, beqtl.summary, bfile, test=c("both","cis","tran
 	my_trans <- paste0(tempnum,'_trans')
     ############
         
-    cmd <- paste0("smr --bfile ", bfile, " --gwas-summary ", gwas.summary, " --beqtl-summary ", beqtl.summary, " --peqtl-smr ", peqtl, " --ld-lower-limit ", ld[1], " --ld-upper-limit ", ld[2], " --peqtl-heidi ", heidi.peqtl, " --heidi-min-m ", heidi.num[1], " --heidi-max-m ", heidi.num[2], " --thread-num ", thread.num)
+    cmd <- paste0("smr --bfile ", bfile, " --gwas-summary ", gwas.summary, " --beqtl-summary ", beqtl.summary, " --peqtl-smr ", peqtl, " --ld-lower-limit ", heidi.ld[1], " --ld-upper-limit ", heidi.ld[2], " --peqtl-heidi ", heidi.peqtl, " --heidi-min-m ", heidi.num[1], " --heidi-max-m ", heidi.num[2], " --thread-num ", thread.num)
     
-    if(test %in% c('cis','both')){
+    if(mode %in% c('cis','both')){
     	cmd_cis <- paste0(cmd, " --cis-wind ", window.cis)
     	if(!heidi){
     		cmd_cis <- paste0(cmd_cis, " --heidi-off ")
@@ -102,22 +92,19 @@ xMEsmr <- function(gwas.summary, beqtl.summary, bfile, test=c("both","cis","tran
 		cmd_cis <- paste0(cmd_cis, " --out ", my_cis, " > my_cis.log")
 		
 		if(verbose){
-			message(sprintf("Analysing (cis) ... (%s)", as.character(Sys.time())), appendLF=T)
+			message(sprintf("cis mode ... (%s)", as.character(Sys.time())), appendLF=TRUE)
+			message(sprintf("%s", cmd_cis), appendLF=TRUE)
 		}
 		flag_cis <- try(system(cmd_cis), silent=TRUE)
-		if(flag_cis==0){
+		if(flag_cis==1){
 			if(verbose){
-				message(sprintf("'%s' executed successfully (%s)", cmd_cis, as.character(Sys.time())), appendLF=TRUE)
-			}
-		}else{
-			if(verbose){
-				message(sprintf("'%s' failed (%s)", cmd_cis, as.character(Sys.time())), appendLF=TRUE)
+				message(sprintf("\tFailed (%s)", as.character(Sys.time())), appendLF=TRUE)
 			}						
 		}
 
     }
     
-    if(test %in% c('trans','both')){
+    if(mode %in% c('trans','both')){
     	cmd_trans <- paste0(cmd, " --trans --trans-wind ", window.trans)
     	if(!heidi){
     		cmd_trans <- paste0(cmd_trans, " --heidi-off ")
@@ -128,14 +115,11 @@ xMEsmr <- function(gwas.summary, beqtl.summary, bfile, test=c("both","cis","tran
 		cmd_trans <- paste0(cmd_trans, " --out ", my_trans, " > my_trans.log")
 		
 		if(verbose){
-			message(sprintf("Analysing (trans) ... (%s)", as.character(Sys.time())), appendLF=T)
+			message(sprintf("trans mode ... (%s)", as.character(Sys.time())), appendLF=TRUE)
+			message(sprintf("%s", cmd_trans), appendLF=TRUE)
 		}
 		flag_trans <- try(system(cmd_trans), silent=TRUE)
-		if(flag_trans==0){
-			if(verbose){
-				message(sprintf("'%s' executed successfully (%s)", cmd_trans, as.character(Sys.time())), appendLF=TRUE)
-			}
-		}else{
+		if(flag_trans==1){
 			if(verbose){
 				message(sprintf("'%s' failed (%s)", cmd_trans, as.character(Sys.time())), appendLF=TRUE)
 			}						
@@ -160,6 +144,20 @@ xMEsmr <- function(gwas.summary, beqtl.summary, bfile, test=c("both","cis","tran
     df_res <- do.call(rbind, list(df_cis, df_trans))
     ###################
     
+    ls_mode <- split(x=df_res, f=df_res$mode)
+    ls_df <- lapply(ls_mode, function(x){
+    	x$fdr_SMR <- stats::p.adjust(x$p_SMR, method=p.adjust.method)
+    	x$fdr_HEIDI <- stats::p.adjust(x$p_HEIDI, method=p.adjust.method)
+    	return(x)
+    })
+    df_output <- do.call(rbind, ls_df)
+    
+    df_output <- df_output[,c('mode','probeID','Gene','ProbeChr','Probe_bp','topSNP','topSNP_chr','topSNP_bp','A1','A2','b_GWAS','b_eQTL','b_SMR','p_GWAS','p_eQTL','p_SMR','fdr_SMR','p_HEIDI','fdr_HEIDI','nsnp_HEIDI')]
+    rownames(df_output) <- NULL
+    
+    p_SMR <- p_GWAS <- NULL
+    df_output <- df_output %>% dplyr::arrange(mode, p_SMR, p_GWAS)
+    
     ####################################################################################
     endT <- Sys.time()
     runTime <- as.numeric(difftime(strptime(endT, "%Y-%m-%d %H:%M:%S"), strptime(startT, "%Y-%m-%d %H:%M:%S"), units="secs"))
@@ -169,5 +167,5 @@ xMEsmr <- function(gwas.summary, beqtl.summary, bfile, test=c("both","cis","tran
     	message(paste(c("Runtime in total (xMEsmr): ",runTime," secs\n"), collapse=""), appendLF=TRUE)
     }
     
-    invisible(df_res)
+    invisible(df_output)
 }
